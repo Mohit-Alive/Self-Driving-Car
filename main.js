b920ffdbf67c411270cb6606b2689cc238884828
mohit-alive <mohitkh22@gmail.com>
2024-06-01 11:13:19 +0530

    Defined the Roads

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100,100,30,50);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1),100,30,50);
 car.draw(ctx);
 
 animate();
@@ -10,6 +11,13 @@ animate();
 function animate(){
     car.update();
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y+canvas.height*0.7);
+
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
